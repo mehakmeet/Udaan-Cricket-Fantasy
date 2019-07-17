ae7e7bd52928414a5c97e6ae9cafafa198daf3c9
MEHAKMEET <mehakmeet.singh2016@vitstudent.ac.in>
2019-07-17 10:48:49 +0530

    final

--- players.js
+++ players.js
@@ -3,45 +3,63 @@ const players=[
 	{
 		p_name:'Karthik',
 		credits:9,
-		points:32,
+		points:12,
 		pos:'WicketKeeper',
 		pid:1
 	},{
-		p_name:'Karthik',
-		credits:9,
-		points:32,
-		pos:'WicketKeeper',
+		p_name:'Rahul',
+		credits:10,
+		points:34,
+		pos:'Bowler',
 		pid:2
 	},{
-		p_name:'Karthik',
-		credits:9,
+		p_name:'Raman',
+		credits:19,
 		points:32,
 		pos:'WicketKeeper',
 		pid:3
 	},{
-		p_name:'Karthik',
-		credits:9,
-		points:32,
+		p_name:'Dhoni',
+		credits:20,
+		points:52,
 		pos:'WicketKeeper',
 		pid:4
 	},{
-		p_name:'Karthik',
-		credits:9,
-		points:32,
-		pos:'WicketKeeper',
+		p_name:'Rohit',
+		credits:16,
+		points:45,
+		pos:'Batsman',
 		pid:5
 	},{
-		p_name:'Karthik',
-		credits:9,
-		points:32,
-		pos:'WicketKeeper',
+		p_name:'Jadeja',
+		credits:17,
+		points:53,
+		pos:'Bowler',
 		pid:6
 	},{
-		p_name:'Karthik',
-		credits:9,
-		points:32,
-		pos:'WicketKeeper',
+		p_name:'Harbhajan',
+		credits:14,
+		points:43,
+		pos:'Bowler',
 		pid:7
+	},{
+		p_name:'Mk d',
+		credits:4,
+		points:3,
+		pos:'Bowler',
+		pid:8
+	},{
+		p_name:'ss shukla',
+		credits:14,
+		points:43,
+		pos:'Bowler',
+		pid:9
+	},{
+		p_name:'Harbhajan',
+		credits:18,
+		points:42,
+		pos:'Bowler',
+		pid:10
 	}
 
 ];
